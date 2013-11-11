96adb0b94225efd2b80e581083344d017ca03720
James Walmsley <james@fullfat-fs.co.uk>
2013-11-11 13:36:57 +0100

    [BT] Added syscalls layer for bt operating system to newlib.

---
+++ newlib/libc/sys/bt/syscalls.c
@@ -0,0 +1,87 @@
+/* note these headers are all provided by newlib - you don't need to provide them */
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/times.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+void _exit() {
+	while(1);
+}
+
+int _close(int file) {
+	return -1;
+}
+
+char **environ; /* pointer to array of char * strings that define the current environment variables */
+
+int _execve(char *name, char **argv, char **env) {
+	return -1;
+}
+
+
+int _fork() {
+	return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+	return -1;
+}
+
+int _getpid() {
+	return 0;
+}
+
+int _isatty(int file) {
+	return -1;
+}
+
+int _kill(int pid, int sig) {
+	return -1;
+}
+
+int _link(char *old, char *new) {
+	return -1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+	return -1;
+}
+
+int _open(const char *name, int flags, ...) {
+	return -1;
+}
+
+int _read(int file, char *ptr, int len) {
+	return -1;
+}
+
+caddr_t _sbrk(int incr) {
+	return 0;
+}
+
+int _stat(const char *file, struct stat *st) {
+	return -1;
+}
+
+clock_t _times(struct tms *buf) {
+	return 0;
+}
+
+int _unlink(char *name) {
+	return -1;
+}
+
+int _wait(int *status) {
+	return -1;
+}
+
+int _write(int file, char *ptr, int len) {
+	return -1;
+}
+
+int _gettimeofday(struct timeval *p, struct timezone *z) {
+	return -1;
+}
