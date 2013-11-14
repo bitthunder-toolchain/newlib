c08100742a3da58f557a993171014155c9f52d0d
James Walmsley <james@fullfat-fs.co.uk>
2013-11-14 20:35:27 +0100

    [BT] Added syscall support for kernel-mode applications - malloc() api and stdio.

---
+++ newlib/libc/sys/bt/bt_api.h
@@ -0,0 +1,16 @@
+#ifndef _BT_API_H_
+#define _BT_API_H_
+
+#include "bt_types.h"
+
+
+extern BT_HANDLE BT_Open(const BT_i8 *name, const BT_i8 *mode, BT_ERROR *pError);
+extern BT_ERROR BT_CloseHandle(BT_HANDLE h);
+
+extern void *BT_kMalloc(BT_u32 size);
+extern void BT_kFree(void *ptr);
+extern void BT_kPrint(const char *fmt, ...);
+
+extern BT_ERROR BT_SetFileDescriptor(BT_u32 n, BT_HANDLE h);
+extern BT_HANDLE BT_GetFileDescriptor(BT_u32 n, BT_ERROR *pError);
+#endif
---
+++ newlib/libc/sys/bt/bt_types.h
@@ -0,0 +1,31 @@
+#ifndef _BT_TYPES_H_
+#define _BT_TYPES_H_
+
+
+#include <stdint.h>
+
+typedef void *				BT_HANDLE;
+
+typedef uint32_t			BT_u32;
+
+typedef char				BT_i8;
+
+typedef BT_u32				BT_ERROR;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
