e75ae2fdaabae509d7950541bc70eb0b9945aefc
James Walmsley <james@fullfat-fs.co.uk>
2013-11-14 22:29:26 +0100

    [BT] Migrated to rentrant syscalls.

--- newlib/libc/sys/bt/syscalls.c
+++ newlib/libc/sys/bt/syscalls.c
@@ -15,33 +15,33 @@ void _exit() {
 }
 
 #ifdef BT_LIBC_TRACE
-#define TP( s, args... )	BT_kPrint(s, ## args)
+#define TP( s, args... )	BT_kPrint("newlib: %s" s, __func__, ## args)
 #else
 #define TP( s, args... )
 #endif
 
-int _open(const char *name, int flags) {
+int _open_r(struct _reent *r, const char *name, int flags) {
 
-	TP("newlib0: _open(%s, %08x);", name, flags);
+	TP("(%s, %08x);", name, flags);
 
 	BT_ERROR Error = 0;
 	BT_HANDLE hFile = BT_Open(name, "rb", &Error);
 
 	if(!hFile) {
-		TP("newlib: _open: Error opening file.");
+		TP("(): Error opening file.");
 		return -1;
 	}
 
-	TP("newlib: _open: Success opening file %p.", hFile);
+	TP("(): Success opening file %p.", hFile);
 
 	BT_SetFileDescriptor(3, hFile);
 
 	return 3;
 }
 
-int _close(int file) {
+int _close_r(struct _reent *r, int file) {
 
-	TP("newlib: _close(%d);", file);
+	TP("(%d);", file);
 
 	BT_ERROR Error = 0;
 	BT_HANDLE hFile = BT_GetFileDescriptor(file, &Error);
@@ -49,61 +49,80 @@ int _close(int file) {
 	BT_CloseHandle(hFile);
 	BT_SetFileDescriptor(file, NULL);
 
-	TP("newlib: _close() succeeded");
+	TP("(): succeeded");
 
 	return 0;
 }
 
 char **environ; /* pointer to array of char * strings that define the current environment variables */
 
-int _execve(char *name, char **argv, char **env) {
+int _execve_r(struct _reent *r, char *name, char **argv, char **env) {
 	return -1;
 }
 
 
-int _fork() {
+int _fork_r(struct _reent *r) {
 	return -1;
 }
 
-int _fstat(int file, struct stat *st) {
+int _fstat_r(struct _reent *r, int file, struct stat *st) {
 	return -1;
 }
 
-int _getpid() {
+int _getpid_r(struct _reent *r) {
 	return 0;
 }
 
-int _isatty(int file) {
+int _isatty_r(struct _reent *r, int file) {
 	return -1;
 }
 
-int _kill(int pid, int sig) {
+int _kill_r(struct _reent *r, int pid, int sig) {
 	return -1;
 }
 
-int _link(char *old, char *new) {
+int _link_r(struct _reent *r, char *old, char *new) {
 	return -1;
 }
 
-int _lseek(int file, int ptr, int dir) {
+int _lseek_r(struct _reent *r, int file, int ptr, int dir) {
 	return -1;
 }
 
-int _read(int file, char *ptr, int len) {
+int _read_r(struct _reent *r, int file, char *ptr, int len) {
 
 	BT_ERROR Error = 0;
 
-	TP("newlib: _read(%d, %p, %d);", file, ptr, len);
+	TP("(%d, %p, %d);", file, ptr, len);
 
 	BT_HANDLE hFile = BT_GetFileDescriptor(file, &Error);
 
 	Error = 0;
 
-	TP("newlib: Read handle %p", hFile);
+	TP("(): Read handle %p", hFile);
 
 	BT_u32 read = BT_Read(hFile, 0, len, ptr, &Error);
 	if(!Error) {
 		return read;
+	}
+
+	return -1;
+}
+
+int _write_r(struct _reent *r, int file, char *ptr, int len) {
+	BT_ERROR Error = 0;
+
+	TP("(%d, %p, %d)", file, ptr, len);
+
+	BT_HANDLE hFile = BT_GetFileDescriptor(file, &Error);
+
+	Error = 0;
+
+	TP("(): Write handle %p", hFile);
+
+	BT_u32 written = BT_Write(hFile, 0, len, ptr, &Error);
+	if(!Error) {
+		return written;
 	}
 
 	return -1;
@@ -114,51 +133,47 @@ int _read(int file, char *ptr, int len) {
 	return 0;
 	}*/
 
-int _stat(const char *file, struct stat *st) {
+int _stat_r(struct _reent *r, const char *file, struct stat *st) {
 	return -1;
 }
 
-clock_t _times(struct tms *buf) {
+clock_t _times_r(struct _reent *r, struct tms *buf) {
 	return 0;
 }
 
-int _unlink(char *name) {
+int _unlink_r(struct _reent *r, char *name) {
 	return -1;
 }
 
-int _wait(int *status) {
+int _wait_r(struct _reent *r, int *status) {
 	return -1;
 }
 
-int _write(int file, char *ptr, int len) {
+int _gettimeofday_r(struct _reent *r, struct timeval *p, struct timezone *z) {
 	return -1;
 }
 
-int _gettimeofday(struct timeval *p, struct timezone *z) {
-	return -1;
-}
-
-void *_malloc_r(struct reent *r, size_t size) {
+void *_malloc_r(struct _reent *r, size_t size) {
 	void *new = BT_kMalloc(size);
-	TP("newlib: _malloc_r(%d) : %p;", size, new);	
+	TP("(%d) : %p;", size, new);	
 	return new;
 }
 
-void _free_r(struct reent *r,  void *ptr) {
-	TP("newlib: _free_r(%p);", ptr);
+void _free_r(struct _reent *r,  void *ptr) {
+	TP("(%p);", ptr);
 	BT_kFree(ptr);
 }
 
-void *_realloc_r(struct reent *r, void *ptr, size_t size) {
-	TP("newlib: _realloc_r(%p, %d);", ptr, size);
+void *_realloc_r(struct _reent *r, void *ptr, size_t size) {
+	TP("(%p, %d);", ptr, size);
 	void *new = BT_kMalloc(size);
 	memcpy(new, ptr, size);
 	BT_kFree(ptr);
 	return new;
 }
 
-void *_calloc_r(struct reent *r, size_t size) {
-	TP("newlib: _calloc_r(%d);", size);
+void *_calloc_r(struct _reent *r, size_t size) {
+	TP("(%d);", size);
 	void *new = BT_kMalloc(size);
 	memset(new, 0, size);
 	return new;
