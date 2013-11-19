476bd1f45dd664acc0fd151e5d7016b6f5d00d32
James Walmsley <james@fullfat-fs.co.uk>
2013-11-19 14:36:48 +0100

    [BT] Added support for conversion of mode flags to bitthunder mode flags.

--- newlib/libc/sys/bt/bt_api.h
+++ newlib/libc/sys/bt/bt_api.h
@@ -3,8 +3,13 @@
 
 #include "bt_types.h"
 
+#define BT_FS_MODE_READ				0x01
+#define	BT_FS_MODE_WRITE			0x02
+#define BT_FS_MODE_APPEND			0x04
+#define	BT_FS_MODE_CREATE			0x08
+#define BT_FS_MODE_TRUNCATE			0x10
 
-extern BT_HANDLE BT_Open(const BT_i8 *name, const BT_i8 *mode, BT_ERROR *pError);
+extern BT_HANDLE BT_Open(const BT_i8 *name, BT_u32 mode, BT_ERROR *pError);
 extern BT_ERROR BT_CloseHandle(BT_HANDLE h);
 
 extern void *BT_kMalloc(BT_u32 size);
@@ -16,4 +21,5 @@ extern BT_s32 BT_AllocFileDescriptor();
 extern BT_ERROR BT_FreeFileDescriptor();
 extern BT_ERROR BT_SetFileDescriptor(BT_u32 n, BT_HANDLE h);
 extern BT_HANDLE BT_GetFileDescriptor(BT_u32 n, BT_ERROR *pError);
+
 #endif
