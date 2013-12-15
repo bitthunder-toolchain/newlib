d943ae143eb875ee185776721f7afc74d7f60479
James Walmsley <james@fullfat-fs.co.uk>
2013-12-15 18:13:04 +0100

    [BITTHUNDER] Fixed userspace api.

--- newlib/libc/sys/bitthunder/bt_types.h
+++ newlib/libc/sys/bitthunder/bt_types.h
@@ -6,24 +6,14 @@
 
 typedef void *				BT_HANDLE;
 
+typedef int64_t				BT_s64;
+
 typedef uint32_t			BT_u32;
+typedef int32_t 			BT_s32;
 
 typedef char				BT_i8;
 
 typedef BT_u32				BT_ERROR;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
