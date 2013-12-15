596fea39c5f7cca2a563e1fc0041c5d325238d43
James Walmsley <james@fullfat-fs.co.uk>
2013-12-15 23:44:43 +0100

    [BITTHUNDER] Integration with userspace syscalls.

--- newlib/libc/sys/bitthunder/bt_api.c
+++ newlib/libc/sys/bitthunder/bt_api.c
@@ -1,12 +1,14 @@
+#include "include/bt_types.h"
 #include <stdlib.h>
-#include "bt_types.h"
+#include "include/bt_syscall.h"
 
 #define WEAK	__attribute__((weak))
 
-
 BT_HANDLE BT_Open(const BT_i8 *name, const BT_i8 *mode, BT_ERROR *pError) WEAK;
 BT_HANDLE BT_Open(const BT_i8 *name, const BT_i8 *mode, BT_ERROR *pError) {
-	return NULL;
+	BT_HANDLE result;
+	DO_SYSCALL(BT_SYS_open, result);
+	return result;
 }
 
 BT_ERROR BT_CloseHandle(BT_HANDLE h) WEAK;
@@ -26,9 +28,20 @@ void BT_kFree(void *ptr) {
 
 void BT_kPrint(const char *fmt) WEAK;
 void BT_kPrint(const char *fmt) {
-#ifndef __thumb__
-	__asm ("svc 2");
-#endif
+	BT_HANDLE result;
+	DO_SYSCALL(BT_SYS_klog, result);
+	return;
+}
+
+
+BT_s32 BT_AllocFileDescriptor() WEAK;
+BT_s32 BT_AllocFileDescriptor() {
+	return 0;
+}
+
+BT_ERROR BT_FreeFileDescriptor() WEAK;
+BT_ERROR BT_FreeFileDescriptor() {
+	return 0;
 }
 
 BT_ERROR BT_SetFileDescriptor(BT_u32 n, BT_HANDLE h) WEAK;
@@ -41,6 +54,10 @@ BT_HANDLE BT_GetFileDescriptor(BT_u32 n, BT_ERROR *pError) {
 	return NULL;
 }
 
+BT_ERROR BT_Seek(BT_HANDLE hFile, BT_s64 ulOffset, BT_u32 whence) WEAK;
+BT_ERROR BT_Seek(BT_HANDLE hFile, BT_s64 ulOffset, BT_u32 whence) {
+	return 0;
+}
 
 BT_u32 BT_Read(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void *pBuffer, BT_ERROR *pError) WEAK;
 BT_u32 BT_Read(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void *pBuffer, BT_ERROR *pError) {
@@ -55,7 +72,14 @@ BT_u32 BT_Write(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, const void *pBuf
 
 BT_ERROR BT_ThreadSleep(BT_u32 ticks) WEAK;
 BT_ERROR BT_ThreadSleep(BT_u32 ticks) {
-#ifndef __thumb__
-	__asm ("svc 3");
-#endif
+	BT_ERROR result;
+	DO_SYSCALL(BT_SYS_sleep, result);
+	return result;
+}
+
+BT_s32 BT_GetProcessID(void) WEAK;
+BT_s32 BT_GetProcessID(void) {
+	BT_s32 result;
+	DO_SYSCALL(BT_SYS_getpid, result);
+	return result;
 }
