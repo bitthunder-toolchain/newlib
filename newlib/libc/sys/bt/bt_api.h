faaa21f6acc90669c05e395a2c10016aaa2e1580
James Walmsley <james@fullfat-fs.co.uk>
2013-12-15 17:51:09 +0100

    [BT] Updated bt sys layer.

--- newlib/libc/sys/bt/bt_api.h
+++ newlib/libc/sys/bt/bt_api.h
@@ -9,21 +9,21 @@
 #define	BT_FS_MODE_CREATE			0x08
 #define BT_FS_MODE_TRUNCATE			0x10
 
-extern BT_ERROR BT_CloseHandle(BT_HANDLE h);
+BT_ERROR BT_CloseHandle(BT_HANDLE h);
 
-extern BT_HANDLE BT_Open(const BT_i8 *name, BT_u32 mode, BT_ERROR *pError);
-extern BT_u32 BT_Read(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void *pBuffer, BT_ERROR *pError);
-extern BT_u32 BT_Write(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, const void *pBuffer, BT_ERROR *pError);
-extern BT_ERROR BT_Seek(BT_HANDLE hFile, BT_s64 ulOffset, BT_u32 whence);
+BT_HANDLE BT_Open(const BT_i8 *name, BT_u32 mode, BT_ERROR *pError);
+BT_u32 BT_Read(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void *pBuffer, BT_ERROR *pError);
+BT_u32 BT_Write(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, const void *pBuffer, BT_ERROR *pError);
+BT_ERROR BT_Seek(BT_HANDLE hFile, BT_s64 ulOffset, BT_u32 whence);
 
-extern void *BT_kMalloc(BT_u32 size);
-extern void BT_kFree(void *ptr);
-extern void BT_kPrint(const char *fmt, ...);
+void *BT_kMalloc(BT_u32 size);
+void BT_kFree(void *ptr);
+void BT_kPrint(const char *fmt, ...);
 
 
-extern BT_s32 BT_AllocFileDescriptor();
-extern BT_ERROR BT_FreeFileDescriptor();
-extern BT_ERROR BT_SetFileDescriptor(BT_u32 n, BT_HANDLE h);
-extern BT_HANDLE BT_GetFileDescriptor(BT_u32 n, BT_ERROR *pError);
+BT_s32 BT_AllocFileDescriptor();
+BT_ERROR BT_FreeFileDescriptor();
+BT_ERROR BT_SetFileDescriptor(BT_u32 n, BT_HANDLE h);
+BT_HANDLE BT_GetFileDescriptor(BT_u32 n, BT_ERROR *pError);
 
 #endif
